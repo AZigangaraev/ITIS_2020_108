9400af217a75d03fd241fa0525f417fb5e05785f
Pasandep <pasandep@yandex.ru>
2020-11-02 20:23:41 +0300

    HW8_Lyapustin_2nd_version

--- Scrolling inst/Scrolling inst/PictureViewController.swift
+++ Scrolling inst/Scrolling inst/PictureViewController.swift
@@ -15,6 +15,7 @@ class PictureViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         scrollView.delegate = self
+        centerImage()
     }
     
     func setImage(image: UIImage) {
@@ -23,5 +24,48 @@ class PictureViewController: UIViewController, UIScrollViewDelegate {
 
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return picture
+    }
+    
+    func setCurrentMaxandMinZoomScale() {
+        let boundsSize = self.view.bounds.size
+        let imageSize = picture.bounds.size
+        
+        let xScale = boundsSize.width / imageSize.width
+        let yScale = boundsSize.height / imageSize.height
+        let minScale = min(xScale, yScale)
+        
+        var maxScale: CGFloat = 1.0
+        if minScale < 0.1 {
+            maxScale = 0.3
+        }
+        if minScale >= 0.1 && minScale < 0.5 {
+            maxScale = 0.7
+        }
+        if minScale >= 0.5 {
+            maxScale = max(1.0, minScale)
+        }
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.maximumZoomScale = maxScale
+    }
+
+    
+    func centerImage() {
+        let boundsSize = self.scrollView.bounds.size
+        var frameToCenter = picture.frame
+        
+        if frameToCenter.size.width < boundsSize.width {
+            frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2
+        } else {
+            frameToCenter.origin.x = 0
+        }
+        
+        if frameToCenter.size.height < boundsSize.height {
+            frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2
+        } else {
+            frameToCenter.origin.y = 0
+        }
+        
+        picture.frame = frameToCenter
     }
 }
