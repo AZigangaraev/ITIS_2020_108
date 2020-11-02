5420f55c9d12e3a529c3cbe3ac339eaf4b174d3c
Rishat <rishatl@inbox.ru>
2020-11-02 20:58:41 +0300

    Latypov_Rishat_hw7
    Все что успел до дедлайна из-за ошибки с делегатом и dataSource. Сейчас после дедлайна добью. Прошу прощения, надеюсь много не снимите)

---
+++ InstagramProfile/InstagramProfile/ImageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ImageViewController.swift
+//  InstagramProfile
+//
+//  Created by Rishat on 02.11.2020.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var zoomImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
